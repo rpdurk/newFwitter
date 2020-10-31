fcf0bc6fc303ddbf52f1a69d5f1c6a196ff93d61
Ryan Durk <rpdurk@gmail.com>
2020-10-31 11:47:11 -0700

    fixed connection production error

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 let connection;
 
 if (process.env.NODE_ENV === 'production') {
-  connection = mysql.createConnection(provess.env.JAWSDB_URL).promise();
+  connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
